4daca909c1a9e61c14f4c932cfe6086239afcdd2
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2025-08-28 17:28:34 +0200

    cleaned repository from unneeded files

--- dist/services/schoolsvc/handler/DeleteSchool/DeleteSchoolCommand.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DeleteSchoolCommand = void 0;
-class DeleteSchoolCommand {
-    constructor(schoolId) {
-        this.schoolId = schoolId;
-    }
-}
-exports.DeleteSchoolCommand = DeleteSchoolCommand;
--- dist/services/schoolsvc/handler/getStudents/GetStudentBySchoolQuery.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GetStudentBySchoolQuery = void 0;
-class GetStudentBySchoolQuery {
-    constructor(schoolId) {
-        this.schoolId = schoolId;
-    }
-}
-exports.GetStudentBySchoolQuery = GetStudentBySchoolQuery;
