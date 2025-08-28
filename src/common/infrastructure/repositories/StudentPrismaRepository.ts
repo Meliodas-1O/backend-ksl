c7342e0f70b6ed8613366b21e907d38a7b76bbe8
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2025-08-28 18:08:14 +0200

    prisma stuff

--- src/common/infrastructure/repositories/StudentPrismaRepository.ts
+++ src/common/infrastructure/repositories/StudentPrismaRepository.ts
@@ -1,99 +1,99 @@
 import { Student } from "common/domain/entities/Student";
 import { IStudentRepository } from "../../../common/domain/repository/IStudentRepository";
-import { PrismaClient } from "../../../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { MapPrismaStudentToDomain } from "../models/PrismaStudent";
 
 const prisma = new PrismaClient();
 export const studentPrismaRepository: IStudentRepository = {
-  findStudentBySchool: function (schoolId: string): Promise<any> {
-    return prisma.student.findMany({
-      where: { schoolId },
-      include: { parent: true, classe: true },
-    });
-  },
-  findStudentByClass: function (classId: string, schoolName: string): Promise<Student | null> {
-    throw new Error("Function not implemented.");
-  },
-  assignStudentToClass: function (
-    student: Student,
-    classId: string,
-    schoolId: string
-  ): Promise<void> {
-    throw new Error("Function not implemented.");
-  },
-  revokeStudentToClass: function (
-    student: Student,
-    classId: string,
-    schoolId: string
-  ): Promise<void> {
-    throw new Error("Function not implemented.");
-  },
-  create: async function (entity: Student): Promise<any> {
-    const data: any = {
-      nom: entity.getNom(),
-      prenom: entity.getPrenom(),
-      dateOfBirth: entity.getDateOfBirth(),
-      abscence: entity.getAbscence(),
-      retards: entity.getRetards(),
-      moyenne: entity.getMoyenne(),
-      classeId: entity.classe,
-      schoolId: entity.schoolId,
-    };
+    findStudentBySchool: function(schoolId: string): Promise<any> {
+        return prisma.student.findMany({
+            where: { schoolId },
+            include: { parent: true, classe: true },
+        });
+    },
+    findStudentByClass: function(classId: string, schoolName: string): Promise<Student | null> {
+        throw new Error("Function not implemented.");
+    },
+    assignStudentToClass: function(
+        student: Student,
+        classId: string,
+        schoolId: string
+    ): Promise<void> {
+        throw new Error("Function not implemented.");
+    },
+    revokeStudentToClass: function(
+        student: Student,
+        classId: string,
+        schoolId: string
+    ): Promise<void> {
+        throw new Error("Function not implemented.");
+    },
+    create: async function(entity: Student): Promise<any> {
+        const data: any = {
+            nom: entity.getNom(),
+            prenom: entity.getPrenom(),
+            dateOfBirth: entity.getDateOfBirth(),
+            abscence: entity.getAbscence(),
+            retards: entity.getRetards(),
+            moyenne: entity.getMoyenne(),
+            classeId: entity.classe,
+            schoolId: entity.schoolId,
+        };
 
-    if (entity.parentId) {
-      data.parentId = entity.parentId;
-    }
+        if (entity.parentId) {
+            data.parentId = entity.parentId;
+        }
 
-    try {
-      console.log("Data", data);
-      const student = await prisma.student.create({
-        data,
-        include: { parent: true, classe: true },
-      });
-      return MapPrismaStudentToDomain(student);
-    } catch (error) {
-      console.error("Failed to create student:", error);
-      throw error;
-    }
-  },
-  update: async function (id: string, entity: Student): Promise<any> {
-    try {
-      const request: any = {
-        nom: entity.getNom(),
-        prenom: entity.getPrenom(),
-        dateOfBirth: entity.getDateOfBirth(),
-        abscence: entity.getAbscence(),
-        retards: entity.getRetards(),
-        moyenne: entity.getMoyenne(),
-      };
-      if (entity.parentId != "") {
-        request.parentId = entity.parentId;
-      }
+        try {
+            console.log("Data", data);
+            const student = await prisma.student.create({
+                data,
+                include: { parent: true, classe: true },
+            });
+            return MapPrismaStudentToDomain(student);
+        } catch (error) {
+            console.error("Failed to create student:", error);
+            throw error;
+        }
+    },
+    update: async function(id: string, entity: Student): Promise<any> {
+        try {
+            const request: any = {
+                nom: entity.getNom(),
+                prenom: entity.getPrenom(),
+                dateOfBirth: entity.getDateOfBirth(),
+                abscence: entity.getAbscence(),
+                retards: entity.getRetards(),
+                moyenne: entity.getMoyenne(),
+            };
+            if (entity.parentId != "") {
+                request.parentId = entity.parentId;
+            }
 
-      if (entity.classe != "") {
-        request.classeId = entity.classe;
-      }
-      console.log("req", request);
-      const updated = await prisma.student.update({
-        where: { id, schoolId: entity.schoolId },
-        include: { parent: true, classe: true },
-        data: request,
-      });
-      return updated;
-    } catch (error) {
-      console.error("Failed to update student:", error);
-      throw error;
-    }
-  },
-  delete: async function (id: string): Promise<void> {
-    try {
-      await prisma.student.delete({ where: { id } });
-    } catch (error) {
-      console.error("Failed to delete student:", error);
-      throw error;
-    }
-  },
-  findStudentById: async function (id: string): Promise<any | null> {
-    return prisma.student.findFirst({ where: { id }, include: { parent: true, classe: true } });
-  },
+            if (entity.classe != "") {
+                request.classeId = entity.classe;
+            }
+            console.log("req", request);
+            const updated = await prisma.student.update({
+                where: { id, schoolId: entity.schoolId },
+                include: { parent: true, classe: true },
+                data: request,
+            });
+            return updated;
+        } catch (error) {
+            console.error("Failed to update student:", error);
+            throw error;
+        }
+    },
+    delete: async function(id: string): Promise<void> {
+        try {
+            await prisma.student.delete({ where: { id } });
+        } catch (error) {
+            console.error("Failed to delete student:", error);
+            throw error;
+        }
+    },
+    findStudentById: async function(id: string): Promise<any | null> {
+        return prisma.student.findFirst({ where: { id }, include: { parent: true, classe: true } });
+    },
 };
